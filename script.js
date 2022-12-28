8140345b0d5345a3252a495b397955e95e5e48d8
blackcommando1301 <100750703+blackcommando1301@users.noreply.github.com>
2022-12-28 08:19:31 +0530

    completed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
  let inputButton=document.querySelector(".DButton");
   let table=document.querySelector(".table");
 
+  
+
 
    inputButton.addEventListener("click",function(){
     ogButton.click();
@@ -9,10 +11,12 @@
 
  ogButton.addEventListener("change",function(e){
     let allFiles=e.target.files;
+    
 
     for(let i=0;i<allFiles.length;i++){
        let newTR=document.createElement("tr");
        console.log(allFiles[i]);
+       let sn=i+1;
        let fileName=allFiles[i].name;
        let fileSize=allFiles[i].size;
        let type=allFiles[i].type;
@@ -31,13 +35,35 @@
         }
           
        newTR.innerHTML=`
-       <th scope="row">${i+1}</th>
-       <td>${fileName}</td>
-       <td>${fileSize}</td>
-       <td><strong>TYPE-</strong>${type} , <strong>Last Modified Date-</strong>${lastModifiedDate}
-       </td>
+       <th scope="row">${sn}</th>
+        <td>${fileName}</td>
+        <td>${fileSize}</td>
+        <td class="infopart">
+          <p class="popup">
+          <strong>TYPE-</strong>${type} , <strong>Last Modified Date-</strong>${lastModifiedDate}
+        </p>
+        <button id="${i+1}" type="button" class="btn btn-outline-danger infoButton">info</button>
+        </td>
        
        `
        table.appendChild(newTR);
+       let infoBtn=newTR.querySelector(".infoButton");
+  let popup=newTR.querySelector(".popup");
+       infoBtn.addEventListener("mouseover",function(e){
+
+        infoBtn.style.display="none";
+        popup.style.display="block";
+    
+        setTimeout(() => {
+            infoBtn.style.display ="flex";
+            popup.style.display="none";
+            popup
+          }, 1000);
+    
+     })
+      
     }
  })
+
+
+ 
